4896481e9ce805e77b88423915a339616789d864
Romario123 <kajdenkoroman35052@gmail.com>
2024-01-27 10:28:33 +1000

    1от 27.01

---
+++ Migrations/20240126233542_AddUserMigration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialNetworkHT.Migrations
+{
+    public partial class AddUserMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>("Image", "AspNetUsers", nullable: true);
+            migrationBuilder.AddColumn<string>("Status", "AspNetUsers", nullable: true);
+            migrationBuilder.AddColumn<string>("About", "AspNetUsers", nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          
+        }
+    }
+}
